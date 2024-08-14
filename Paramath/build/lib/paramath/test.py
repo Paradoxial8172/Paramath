22cf8e77fbe5180316e8eeee1634a57dd68471a4
Tamer Alssaleh <52636947+Paradoxial8172@users.noreply.github.com>
2024-08-14 15:21:18 +0300

    Delete Paramath directory

--- Paramath/build/lib/paramath/test.py
+++
@@ -1,9 +0,0 @@
-# TEST PY FILE
-from main import *
-
-data = DataSet([1,3,1,4,1,4,1,4,5,61,44,2342,24,44,55,1,4,0,1,3])
-number = 124124123123
-floatnumber = 14123.1324121
-string = "adcnjaasfafaffaefee"
-
-print(System.encode("Hello, World!", BASE32))
